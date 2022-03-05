169be0821fbca0b6895337a7a5ba92df29d3379f
Vadim Artyushenko <artushenkomail@mail.ru>
2022-03-06 00:34:16 +0700

    Добавьте файлы проекта.

---
+++ BlazorServerApp/Features/Tweets/List.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+namespace BlazorServerApp.Features.Tweets;
+
+public class List
+{
+    public class Query : IRequest<Model>
+    {
+        
+    }
+    
+    public class Model
+    { 
+        public List<string> Tweets { get; set; }
+    }
+    
+    public class QueryHandler : IRequestHandler<Query, Model>
+    {
+        public async Task<Model> Handle(Query request, CancellationToken cancellationToken)
+        {
+            return new Model
+            {
+                Tweets = new List<string>
+                {
+                    "One from server",
+                    "Two from server",
+                    "Three from server",
+                    "Four"
+                }
+            };
+        }   
+        
+    }
+}
